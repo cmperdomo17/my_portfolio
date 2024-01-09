fac33a1f68944e3554ba12d6a533ae40ba8a1e1b
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-01-09 00:06:13 -0500

    Update Navbar styling and links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,22 +6,22 @@ const NavBar = () => {
 
     return (
         <div>
-            <nav className={`flex justify-between items-center pt-2 px-20 ${darkMode ? 'bg-slate-950 text-white' : 'bg-slate-300 text-slate-950'}`}>
+            <nav className={`flex justify-between items-center pt-4 px-20 ${darkMode ? 'bg-slate-950 text-white' : 'bg-slate-300 text-slate-950'}`}>
 
                 <div className="font-ubuntu-mono font-bold text-center cursor-pointer">
                     <h1 className="text-3xl hover:scale-95 transition-all ease-out ">
-                        Mario
+                        &lt; MarioDev /&gt;
                     </h1>
                 </div>
 
-                <div className="flex items-center space-x-4 ">
-                    <a href="#about" className="font-ubuntu-mono font-bold text-lg hover:scale-95 transition-all ease-out">
+                <div className="flex items-center space-x-10 font-ubuntu-mono font-bold text-xl">
+                    <a href="#about" className=" hover:scale-95 transition-all ease-out">
                         About
                     </a>
-                    <a href="#projects" className="font-ubuntu-mono font-bold text-lg hover:scale-95 transition-all ease-out">
+                    <a href="#projects" className="hover:scale-95 transition-all ease-out">
                         Projects
                     </a>
-                    <a href="#contact" className="font-ubuntu-mono font-bold text-lg hover:scale-95 transition-all ease-out">
+                    <a href="#contact" className="hover:scale-95 transition-all ease-out">
                         Contact
                     </a>
                 </div>
