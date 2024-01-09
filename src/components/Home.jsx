3ecf9874c692adefc1cef1a1d18e0b58782967d4
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-01-09 15:27:38 -0500

    Refactor dark mode toggle and add social media icons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,19 @@
 import React, { useContext } from "react";
-import { FaSun, FaMoon } from "react-icons/fa";
 import { ThemeContext } from './Theme';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Home = () => {
-    const { darkMode, handleClick } = useContext(ThemeContext);
+    const { darkMode } = useContext(ThemeContext);
 
     return (
     
     <div className={`flex flex-col justify-center items-center h-screen ${darkMode ? 'bg-gradient-to-b from-black to-slate-950' : 'bg-gradient-to-b from-slate-300 to-slate-200'}`}>
-        
-        <button 
-            onClick={handleClick} 
-            className={`p-2 m-4 rounded-full shadow hover:scale-95 transition-all duration-200 ${darkMode ? 'bg-gray-700 text-white' : 'bg-blue-500 text-white'}`}>
-            {darkMode ? <FaMoon size={25} /> : <FaSun size={25} />}
-        </button>
 
         <div className="flex justify-center py-4">
             <img 
                 src={`${process.env.PUBLIC_URL}/me2.png`} 
                 alt="Mario" 
-                className={`w-52 h-52 rounded-full border-4 hover:scale-105 transition-all ease-out ${darkMode ? 'bg-blue-500' : 'bg-slate-200 border-blue-500'}`}
+                className={`w-52 h-52 rounded-full border-4 hover:scale-105 transition-all ease-out ${darkMode ? 'bg-blue-400' : 'bg-slate-200 border-blue-500'}`}
             />
         </div>
 
@@ -27,7 +21,7 @@ const Home = () => {
 
             <div className="flex justify-center">
                 <h1 className={`text-7xl ${darkMode ? 'text-white' : 'text-slate-900'} hover:scale-110 transition-all ease-out animate-typing1 animation-fill-forwards overflow-hidden border-r-4 w-12 text-nowrap`}>
-                    Hi, I'm <span className="text-blue-500">Mario!</span>
+                    Hi, I'm <span className={`${darkMode ? 'text-blue-400' : 'text-blue-600'}`}>Mario!</span>
                 </h1>
             </div>
             
@@ -37,17 +31,29 @@ const Home = () => {
                 </h1>
             </div>
 
-            <div className="flex justify-center pt-16 ">
-                <p className={`text-xl hover:scale-110 transition-all ease-out ${darkMode ? 'text-white' : 'text-slate-900'}`}>
-                    I'm a software developer with a passion for learning new technologies and building cool stuff.
-                </p>
+            <div className="flex justify-center pt-14 ">
+                <div className={`text-xl hover:scale-110 transition-all ease-out ${darkMode ? 'text-white' : 'text-slate-900'}`}>
+                    <p>
+                        I'm a software developer with a passion for learning new technologies and building cool stuff.
+                    </p>
+                    <p>
+                        I'm a seventh semester Systems Engineering student at <a href="https://www.unicauca.edu.co/" className={`${darkMode ? 'text-blue-400' : 'text-blue-600'} hover:underline`}>University of Cauca</a>.
+                    </p>
+                </div>
             </div>
 
-            <div className="flex justify-center pb-12">
-                <button className={`p-2 px-4 m-4 text-xl rounded-full hover:scale-90 transition-all duration-200 bg-blue-500 text-white `}>
+            <div className="flex justify-center pb-12 pt-4">
+                <button className={`p-2 px-4 m-4 text-xl rounded-full hover:scale-110 transition-all duration-200 ${darkMode ? 'bg-blue-400 text-slate-900' : 'bg-blue-600 text-white'}`}>
                     Contact Me!
                 </button>
+                <a href="https://www.linkedin.com/in/carlos-mario-perdomo-ramos-665a8a276/" target="_blank" rel="noreferrer" className={`p-2 px-4 m-4 text-xl rounded-full hover:scale-125 transition-all duration-200 ${darkMode ? 'bg-blue-400 text-slate-900' : 'bg-blue-600 text-white'} flex items-center justify-center`}>
+                    <FaLinkedin size={25} />
+                </a>
+                <a href="https://github.com/cmperdomo17" target="_blank" rel="noreferrer" className={`p-2 px-4 m-4 text-xl rounded-full hover:scale-125 transition-all duration-200 ${darkMode ? 'bg-blue-400 text-slate-900' : 'bg-blue-600 text-white'} flex items-center justify-center`}>
+                    <FaGithub size={25} />
+                </a>
             </div>
+
 
         </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from "react";
 import { ThemeContext } from './Theme';
+import { FaSun, FaMoon } from "react-icons/fa";
 
 const NavBar = () => {
-    const { darkMode } = useContext(ThemeContext);
+    const { darkMode, handleClick } = useContext(ThemeContext);
 
     return (
         <div>
@@ -10,20 +11,26 @@ const NavBar = () => {
 
                 <div className="font-ubuntu-mono font-bold text-center cursor-pointer">
                     <h1 className="text-2xl hover:scale-110 transition-all ease-out ">
-                        &lt; mario.<span className="text-blue-500">dev </span>/&gt;
+                        &lt; mario.<span className={`${darkMode ? 'text-blue-300' : 'text-blue-600' }`}>dev </span>/&gt;
                     </h1>
                 </div>
 
                 <div className="flex items-center space-x-12 font-ubuntu-mono font-bold text-lg">
-                    <a href="#about" className=" hover:scale-125 transition-all ease-out">
+                    
+                    <a href="#about" className={`hover:scale-125  ${darkMode ? 'hover:text-blue-400' : 'hover:text-blue-600' } transition-all ease-out`}>
                         About
                     </a>
-                    <a href="#projects" className="hover:scale-125 transition-all ease-out">
+                    <a href="#projects" className={`hover:scale-125  ${darkMode ? 'hover:text-blue-400' : 'hover:text-blue-600' } transition-all ease-out`}>
                         Projects
                     </a>
-                    <a href="#contact" className="hover:scale-125 transition-all ease-out">
+                    <a href="#contact" className={`hover:scale-125  ${darkMode ? 'hover:text-blue-400' : 'hover:text-blue-600' } transition-all ease-out`}>
                         Contact
                     </a>
+                    <button 
+                        onClick={handleClick} 
+                        className={`p-2 rounded-full ${darkMode ? 'bg-gray-700 text-white' : 'bg-blue-500 text-white'}`}>
+                        {darkMode ? <FaMoon size={20} /> : <FaSun size={20} />}
+                    </button>
                 </div>
             </nav>
         </div>
