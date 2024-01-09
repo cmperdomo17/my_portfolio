a0e4c8d5db07d0965b9bcfc5af934a846a5a2fc6
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-01-09 00:23:24 -0500

    Add handleClick function to toggle dark mode

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -5,6 +5,7 @@ export const ThemeContext = createContext();
 export const ThemeProvider = ({ children }) => {
     const [darkMode, setDarkMode] = useState(false);
 
+    // eslint-disable-next-line no-unused-vars
     const handleClick = () => {
         setDarkMode(!darkMode);
     };
