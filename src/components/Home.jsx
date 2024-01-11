163c6aa15be9a0465ec4b1e36357e27464ccbf80
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-01-10 23:01:10 -0500

    Update Home component styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
     return (
     
-    <div className={`flex flex-col justify-center h-[100vh] md:h-[110vh] ${darkMode ? 'bg-gradient-to-b from-black to-slate-950' : 'bg-gradient-to-b from-slate-300 to-slate-200'}`}>
+    <div className={`flex flex-col justify-center h-[110vh] ${darkMode ? 'bg-gradient-to-b from-black to-slate-950' : 'bg-gradient-to-b from-slate-300 to-slate-200'}`}>
         
         <div className="flex justify-center pb-4">
             <img 
