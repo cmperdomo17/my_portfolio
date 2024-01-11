dcf8986b85171895624b6696513d6332c9d6f952
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-01-10 23:09:01 -0500

    Update Home component styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,32 +7,32 @@ const Home = () => {
 
     return (
     
-    <div className={`flex flex-col justify-center h-[110vh] ${darkMode ? 'bg-gradient-to-b from-black to-slate-950' : 'bg-gradient-to-b from-slate-300 to-slate-200'}`}>
+    <div className={`flex flex-col justify-center h-[105vh] md:h-[110vh] ${darkMode ? 'bg-gradient-to-b from-black to-slate-950' : 'bg-gradient-to-b from-slate-300 to-slate-200'}`}>
         
         <div className="flex justify-center pb-4">
             <img 
                 src={`${process.env.PUBLIC_URL}/me2.png`} 
                 alt="Mario" 
-                className={`w-52 h-52 rounded-full border-4 hover:scale-105 transition-all ease-out ${darkMode ? 'bg-blue-400' : 'bg-slate-200 border-blue-500'}`}
+                className={`w-32 h-32 md:w-52 md:h-52 rounded-full border-4 hover:scale-105 transition-all ease-out ${darkMode ? 'bg-blue-400' : 'bg-slate-200 border-blue-500'}`}
             />
         </div>
 
         <div className="font-ubuntu-mono font-bold text-center">
 
             <div className="flex justify-center">
-                <h1 className={`text-6xl ${darkMode ? 'text-white' : 'text-slate-900'} animate-typing1 animation-fill-forwards overflow-hidden border-r-4 w-12 text-nowrap`}>
+                <h1 className={`text-2xl md:text-6xl ${darkMode ? 'text-white' : 'text-slate-900'} animate-typing1 animation-fill-forwards overflow-hidden border-r-4 w-12 text-nowrap`}>
                     Hi, I'm <span className={`${darkMode ? 'text-blue-400' : 'text-blue-600'}`}>Mario!</span>
                 </h1>
             </div>
             
             <div className="flex justify-center">
-                <h1 className={`text-3xl animate-typing2 animation-fill-forwards ${darkMode ? 'text-white' : 'text-slate-900'} overflow-hidden border-r-4 w-25 text-nowrap`}>
+                <h1 className={`text-lg md:text-3xl animate-typing2 animation-fill-forwards ${darkMode ? 'text-white' : 'text-slate-900'} overflow-hidden border-r-4 w-25 text-nowrap`}>
                     Systems Engineer
                 </h1>
             </div>
 
             <div className="pt-10">
-                <div className={`text-xl font-sans ${darkMode ? 'text-white' : 'text-slate-900'}`}>
+                <div className={`text-base md:text-xl font-sans ${darkMode ? 'text-white' : 'text-slate-900'}`}>
                     <p>
                         I'm a software developer with a passion for learning new technologies and building cool stuff.
                     </p>
