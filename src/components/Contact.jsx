112992c0611824c93649147e7617c30db64f2a07
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-02-01 17:11:02 -0500

    Actualizar estilos de Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -45,7 +45,7 @@ const Contact = () => {
             </div>
 
             {/* Text Area and Inputs */}
-            <div className="flex justify-center flex-col md:flex-row items-center md:max-w-[700px] mx-auto pt-14 gap-10 pb-20">
+            <div className="flex justify-center flex-col md:flex-row items-center md:max-w-[700px] mx-auto pt-10 gap-4 pb-20">
                 <div className="flex flex-col items-center">
                     <input placeholder="Username" className={`m-4 bg-transparent border-none outline-none max-w-xs px-5 py-2 text-lg font-Onest rounded-full shadow-inner  ${darkMode ? 'text-white shadow-blue-400 border-blue-400' : 'text-slate-900 shadow-slate-500'}`} name="text" type="text" />
                     <input placeholder="Email" className={` bg-transparent border-none outline-none max-w-xs px-5 py-2 text-lg font-Onest rounded-full shadow-inner  ${darkMode ? 'text-white shadow-blue-400 border-blue-400' : 'text-slate-900 shadow-slate-500'}`} name="text" type="text" />
@@ -53,7 +53,10 @@ const Contact = () => {
 
                 <div className="flex flex-col md:flex-row items-center">
                     <textarea placeholder="Message" className={` mx-2 bg-transparent border-none outline-none overflow-auto max-w-xs px-10 py-6 md:p-6 text-lg text-pretty font-Onest rounded-3xl shadow-inner resize-none  ${darkMode ? 'text-white shadow-blue-400 border-blue-400' : 'text-slate-900 shadow-slate-500'}`} name="text" type="text" />
-                    <button className={`p-2 px-4 m-4 text-lg font-Onest font-semibold rounded-full hover:scale-100 md:hover:scale-110 transition-all duration-500 ${darkMode ? 'bg-blue-400 text-slate-900' : 'bg-blue-600 text-white'}`}>
+                    <button 
+                        className={`p-2 px-4 m-4 text-lg font-Onest font-semibold rounded-full hover:scale-100 md:hover:scale-110 transition-all duration-500 ${darkMode ? 'bg-blue-400 text-slate-900' : 'bg-blue-600 text-white'}`} 
+                        onClick={() => alert("Mensaje enviado ")}
+                    >
                         Send
                     </button>
                 </div>
